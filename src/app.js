ab7ef58f84df7601c8f53cf36833fca2af245a04
Aum Soni <96745193+aumsoni2002@users.noreply.github.com>
2026-09-17 18:19:56 -0400

    Secure our API with Arcjet

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import authRoutes from '#routes/auth.routes.js';
+import securityMiddleware from '#middleware/security.middleware.js';
 
 const app = express();
 
@@ -14,11 +15,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(
-  morgan('combined', {
-    stream: { write: message => logger.info(message.trim()) },
-  })
-);
+app.use(morgan('combined', { stream: { write: (message) => logger.info(message.trim()) }}));
+
+app.use(securityMiddleware);
 
 app.get('/', (req, res) => {
   logger.info('Hello from Acquisitions!');
@@ -27,13 +26,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/health', (req, res) => {
-  res
-    .status(200)
-    .json({
-      status: 'OK',
-      timestamp: new Date().toISOString(),
-      uptime: process.uptime(),
-    });
+  res.status(200).json({ status: 'OK', timestamp: new Date().toISOString(), uptime: process.uptime() });
 });
 
 app.get('/api', (req, res) => {
